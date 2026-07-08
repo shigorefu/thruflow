e012850e3e92e609438f7c6d18846283fa6fc813
Edward Shchigolev <shigorefu@icloud.com>
2026-07-08 15:08:16 +0900

    Initial Commit

---
+++ ThruFlowTests/ThruFlowTests.swift
@@ -0,0 +1,18 @@
+//
+//  ThruFlowTests.swift
+//  ThruFlowTests
+//
+//  Created by エドワード on 2026/07/08.
+//
+
+import Testing
+
+struct ThruFlowTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
